514da8de07cdf9537745e5d78fdd7512299ec7ee
justcodingnepal <your_new_email@example.com>
2025-04-05 12:25:55 +0545

    added local storage on first load it shows iron man

--- movie.js
+++ movie.js
@@ -1,31 +1,35 @@
-document.getElementById('search-movie').addEventListener('click',fetch_movie);
-
-
-async function fetch_movie() {
-    let movie_name=document.getElementById("search_bar").value
-    const response = await fetch(`http://www.omdbapi.com/?t=${movie_name}&apikey=2d21d2e`)
-    const data = await response.json()
-    if(!data.Error){
-        document.getElementById("movie_tit").innerHTML=`${data.Title}`
-        document.getElementById("movie_yr").innerHTML=`${data.Year}`
-        document.getElementById("movie_rat").innerHTML=`${data.imdbRating}`
-        document.getElementById("movie-image").src = data.Poster;
-        document.getElementById("movie_genre").innerHTML=`${data.Genre}`
-        document.getElementById("movie_actor").innerHTML=`${data.Actors}`
-        document.getElementById("movie_writer").innerHTML=`${data.Writer}`
-        document.getElementById("movie_language").innerHTML=`${data.Language}`
-        document.getElementById("movie_director").innerHTML=`${data.Director}`
-    }else{
-        document.querySelector('.container').innerHTML = 
-            `<div style="width: 100%; 
-                         text-align: center;
-                         padding: 15px; 
-                         font-size: 24px;
-                         font-family: 'Courier New', Courier, monospace;
-                         display: flex;
-                         justify-content: center;
-                         align-items: center;">
-                Oops! Movie not found
-             </div>`;
+async function loadMovie() {
+    let savedMovie = localStorage.getItem("movieName");
+    if (savedMovie) {
+        fetchMovieByName(savedMovie);
+    } else {
+        fetchMovieByName("Iron Man");
     }
 }
+async function fetchMovieByName(movieName) {
+    const response = await fetch(`http://www.omdbapi.com/?t=${movieName}&apikey=2d21d2e`);
+    const data = await response.json();
+    
+    if (!data.Error) {
+        updateMovieData(data);
+    } else {
+        alert("Movie not found!");
+    }
+}
+function updateMovieData(data) {
+    document.getElementById("movie_tit").innerHTML = `${data.Title}`;
+    document.getElementById("movie_yr").innerHTML = `${data.Year}`;
+    document.getElementById("movie_rat").innerHTML = `${data.imdbRating}`;
+    document.getElementById("movie-image").src = data.Poster;
+    document.getElementById("movie_genre").innerHTML = `${data.Genre}`;
+    document.getElementById("movie_actor").innerHTML = `${data.Actors}`;
+    document.getElementById("movie_writer").innerHTML = `${data.Writer}`;
+    document.getElementById("movie_language").innerHTML = `${data.Language}`;
+    document.getElementById("movie_director").innerHTML = `${data.Director}`;
+}
+document.getElementById('search-movie').addEventListener('click', () => {
+    let movie_name = document.getElementById("search_bar").value;
+    localStorage.setItem("movieName", movie_name);
+    fetchMovieByName(movie_name);
+});
+window.onload = loadMovie;
